7a43d57d32a7c7f9387f4565b4e35d01f914a63a
Harald Sack <harald.sack@t-systems.com>
2025-06-06 11:54:41 +0200

    check version

---
+++ .github/workflows/scripts/checkPackageVersion.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import json
+
+# Define the necessary variables
+# see: https://docs.github.com/en/rest/packages/packages?apiVersion=2022-11-28#list-package-versions-for-a-package-owned-by-a-user
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+USER = os.environ.get("USER")
+PACKAGE_TYPE = os.environ.get("PACKAGE_TYPE", "maven")
+PACKAGE_NAME = os.environ.get("PACKAGE_NAME")
+PACKAGE_VERSION_NAME = os.environ.get("PACKAGE_VERSION")
+
+HEADERS = {
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github.v3+json",
+}
+
+url = f"https://api.github.com/users/{USER}/packages/{PACKAGE_TYPE}/{PACKAGE_NAME}/versions"
+
+# Make the GET request to the API
+response = requests.get(url, headers=HEADERS)
+
+exists = "false"
+
+# Check if the request was successful
+if response.status_code == 200:
+    packages = json.loads(response.text)
+
+    for package in packages:
+        if package["name"] == PACKAGE_VERSION_NAME:
+            exists = "true"
+            break
+
+with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+    fh.write(f"exists={exists}\n")
